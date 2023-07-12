788c6cff121942648f73995aab8be4b92f829d24
Agostina Celina Venezia Napolillo <112207761+AgosVenezia@users.noreply.github.com>
2023-07-12 14:09:59 -0300

    Test cases de Buggy Cars Rating automatizados con Rapise

---
+++ AgosVenezia1 TC13631/Objects.js
@@ -0,0 +1,210 @@
+var saved_script_objects={
+	"MY-APP": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "\n  \n    \n      Buggy Rating\n      \n    \n        \n            \n            \n            \n        \n        Login\n        R...",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "MY-APP",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//my-app",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/"
+	},
+	"Register": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Register",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//a[@href='/register']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/"
+	},
+	"Login": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Login",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='username' and @name='username' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"First_Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "First Name",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='firstName' and @name='firstName' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Last_Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Last Name",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='lastName' and @name='lastName' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Password": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Password",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='password' and @name='password' and @type='password']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Confirm_Password": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Confirm Password",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='confirmPassword' and @name='confirmPassword' and @type='password']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Register1": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Register",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//div/div/form/button[@type='submit']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	}
+};
---
+++ Agostina1 TC12909/Objects.js
@@ -0,0 +1,279 @@
+var saved_script_objects={
+	"Google_Chrome:_1_ventana_en_ejec": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "param:window_class"
+				}
+			}
+		],
+		"window_class": "Shell_TrayWnd",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Google Chrome: 1 ventana en ejecución",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "",
+		"location": "Barra de tareas/Pane/Aplicaciones en ejecución/Aplicaciones en ejecución/Chrome.scopeddir83561133086826.Default"
+	},
+	"Register": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Register",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//a[@href='/register']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/"
+	},
+	"Login": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Login",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='username' and @name='username' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"First_Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "First Name",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='firstName' and @name='firstName' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Last_Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Last Name",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='lastName' and @name='lastName' and @type='text']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Password": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Password",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='password' and @name='password' and @type='password']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Confirm_Password": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Confirm Password",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//input[@id='confirmPassword' and @name='confirmPassword' and @type='password']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Register1": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Register",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//div/div/form/button[@type='submit']",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	},
+	"Google_Chrome:_1_ventana_en_ejec1": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Button"
+						},
+						{
+							"property": "Name",
+							"value": "Google Chrome: 1 ventana en ejecución"
+						},
+						{
+							"property": "AutomationId",
+							"value": "Chrome"
+						}
+					],
+					"window_name": "",
+					"window_class": "param:window_class"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "param:window_class"
+				}
+			}
+		],
+		"window_class": "Shell_TrayWnd",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Google Chrome: 1 ventana en ejecución",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "",
+		"location": "Barra de tareas/Pane/Aplicaciones en ejecución/Aplicaciones en ejecución/Chrome"
+	},
+	"MY-REGISTER": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "\n    \n        Register with Buggy Cars Rating\n\n        \n            \n                Login\n                \n            ...",
+		"object_role": "",
+		"object_class": "Selenium",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "MY-REGISTER",
+		"ignore_object_name": true,
+		"object_library": "Selenium",
+		"window_name": "Buggy Cars Rating",
+		"xpath": "//my-register",
+		"title": "Buggy Cars Rating",
+		"url": "https://buggy.justtestit.org/register"
+	}
+};
